0543120113590a4e0685d77a2155fbfac9708f39
George <santos.s.george@gmail.com>
2019-09-19 21:29:28 -0300

    creating project spring-mvc-example

---
+++ gestao-titulo/src/main/java/com/company/titulo/GestaoTituloApplication.java
@@ -0,0 +1,13 @@
+package com.company.titulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTituloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTituloApplication.class, args);
+	}
+
+}
